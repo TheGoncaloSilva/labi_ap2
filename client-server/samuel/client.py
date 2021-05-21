1f19d52133347881baf53e9c2f1f8ac04915043f
Samuel Teixeira <steixeira@ua.pt>
2021-05-21 02:00:02 +0100

    Alguns progressos na função run_client

--- client-server/samuel/client.py
+++ client-server/samuel/client.py
@@ -11,24 +11,26 @@ from Crypto.Cipher import AES
 
 # Função para encriptar valores a enviar em formato jsos com codificação base64
 # return int data encrypted in a 16 bytes binary string coded in base64
-def encrypt_intvalue (cipherkey, data):
-	return None
+
+
+def encrypt_intvalue(cipherkey, data):
+    return None
 
 
 # Função para desencriptar valores recebidos em formato json com codificação base64
 # return int data decrypted from a 16 bytes binary strings coded in base64
-def decrypt_intvalue (cipherkey, data):
-	return None
+def decrypt_intvalue(cipherkey, data):
+    return None
 
 
 # verify if response from server is valid or is an error message and act accordingly
-def validate_response (client_sock, response):
-	return None
+def validate_response(client_sock, response):
+    return None
 
 
 # process QUIT operation
-def quit_action (client_sock, attempts):
-	return None
+def quit_action(client_sock, attempts):
+    return None
 
 
 # Outcomming message structure:
@@ -47,69 +49,100 @@ def quit_action (client_sock, attempts):
 #
 # Suporte da execução do cliente
 #
-def run_client (client_sock, client_id):
-	return None
-	
+def run_client(client_sock, client_id):
+    tries = 0
+    print("Bem vindo aoooooooooo Adivinha o número secreto!!!!")
+    while True:
+        start = {'op': "START", 'id': client_id, [cipher]: cypherkey}
+        recvstart = sendrecv_dict(client_sock, start)
+
+        # Menu
+        print("O que pretende fazer?")
+        print("Adivinhar - 1")
+        print("Terminar o jogo - 2")
+        print("Sair - 3")
+
+        option = int(input(""))
+        if option == 1:
+            num = int(input("Adivinhe o número secreto:"))
+            guess = {'op': "GUESS", 'number': num}
+            recvguess = sendrecv_dict(client_sock, guess)
+            tries += 1
+            if recvguess['result'] == "equals" and tries <= 30:
+                print("Ganhou!")
+            elif recvguess['result'] == "smaller":
+                print("O número secreto é menor do que o inserido")
+            elif recvguess['result'] == "bigger":
+                print("O número secreto é maior do que o inserido")
+            else:
+                print(
+                    "Perdeu! Adivinhou o número, mas ultrapassou o máximo de tentativas")
+
+        if option == 2:
+            lastnum = int(input("Tente uma útlima vez:"))
+            stop = {"op": "STOP", "number": lastnum, "attempts": tries}
+            recvstop = sendrecv_dict(client_sock, stop)
+            tries += 1
+            if stop[num] == recvstop[guess] and tries <= 30:
+                print(f"O número era {recvstop[guess]} e você ganhou!")
+                
+
+        if option == 3:
+            quit = {"op": "QUIT"}
+            recvquit = sendrecv_dict(client_sock, quit)
+            print("Saiu do jogo")
+            break
+
+    return None
+
 
 def main():
-	# validate the number of arguments and eventually print error message and exit with error
-	if len(sys.argv) != 4:
-		print("Argumentos inválidos, deve ter o formato:")
-		print("python3 client.py client_id porto [máquina]")
-		sys.exit
-	# verify type of of arguments and eventually print error message and exit with error
-    
-	#Verifica a validade do id do cliente
-	if any(char.isdigit() for char in sys.argv[1]) or len(sys.argv[1]) <= 0:
-		print("ID de client inválido!")
-		sys.exit
-    
-	#Verifica a validade da porta
-	for i in range(0, len(sys.argv[2]) - 1):
-		if sys.argv[2][i].isalpha:
-			print("Porta inválida! A porta não deve conter letras")
-			sys.exit
-    
-	if sys.argv[2] > 65535 or sys.argv[2] < 0:
-		print("Porta inválida! Deve escrever um número entre 0 e 65535")
-		sys.exit
-    
-	#Verifica a validade da máquina
-	host = sys.argv[3].split('.')
-	for i in range(0, len(host) - 1):
-         if host[i] < 0 or host[i] > 255:
-            print("Erro! A máquina é identificada da seguinte maneira:")
-			print("X.X.X.X ,sendo X um número entre 0 e 255")
-			sys.exit
+    # validate the number of arguments and eventually print error message and exit with error
+    if len(sys.argv) != 4:
+        print("Argumentos inválidos, deve ter o formato:")
+        print("python3 client.py client_id porto [máquina]")
+        sys.exit
+    # verify type of of arguments and eventually print error message and exit with error
 
-    #127.0.0.1
+    # Verifica a validade do id do cliente
+    if any(char.isdigit() for char in sys.argv[1]) or len(sys.argv[1]) <= 0:
+        print("ID de client inválido!")
+        sys.exit
+
+    # Verifica a validade da porta
+    for i in range(0, len(sys.argv[2]) - 1):
+        if sys.argv[2][i].isalpha:
+            print("Porta inválida! A porta não deve conter letras")
+            sys.exit
+
+    if sys.argv[2] > 65535 or sys.argv[2] < 0:
+        print("Porta inválida! Deve escrever um número entre 0 e 65535")
+        sys.exit
+
+    # Verifica a validade da máquina
+    host = sys.argv[3].split('.')
+    for i in range(0, len(host) - 1):
+        if host[i] < 0 or host[i] > 255:
+            print("Erro! A máquina é identificada da seguinte maneira:")
+            print("X.X.X.X ,sendo X um número entre 0 e 255")
+            sys.exit
+
+    # 127.0.0.1
     port = sys.argv[2]
 
-	hostname = sys.argv[3]
-    
-	#Socket
-	client_sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+    hostname = sys.argv[3]
 
-	#Ligar ao servidor
-	client_sock.connect ((hostname, port))
+    # Socket
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	start = { "op": "START"}
-	start["client_id"] = input("Nome: ")
+    # Ligar ao servidor
+    client_sock.connect((hostname, port))
 
-	#
-	if any(char.isdigit() for char in start["client_id"]) or len(start["client_id"]) <= 0:
-		print("ID de client inválido!")
-	else:
-	    
-        x = client_sock.sendrecv_dict(hostname, start)
-	    if x = None:
-	        print("Erro")
-	  
+    run_client(client_sock, sys.argv[1])
 
-	run_client (client_sock, sys.argv[1])
+    client_sock.close()
+    sys.exit(0)
 
-	client_sock.close ()
-	sys.exit (0)
 
 if __name__ == "__main__":
     main()
